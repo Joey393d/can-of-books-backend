9cedaf8fd6ad9f65706086f3d5d2dae0f31ba5e6
Joey <joseph.r.young2002@gmail.com>
2021-09-13 15:55:06 -0500

    added book path and seed.js

--- server.js
+++ server.js
@@ -7,9 +7,36 @@ const mongoose = require('mongoose');
 
 
 var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('mongo is now connected!');
+});
+
+
+mongoose.connect(process.env.MONGODB_URL);
+
+
+const Book = require('./models/book');
 
 const app = express();
 app.use(cors());
+
+
+app.get('/books', async (req, res) => {
+  const location = req.query.location;
+
+  const findQuery = {};
+  if (location) {
+    findQuery.location = location;
+  }
+  const books = await Book.find(findQuery);
+
+  res.send(books);
+
+})
+
+
+
 
 const PORT = process.env.PORT || 3001;
 
